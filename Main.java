96c0b5afabfbc5d30d3482b13bdd398a4ab5cb34
san9901 <san990190@gmail.com>
2024-11-20 09:33:02 +0900

    three

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
     System.out.println("202014101 김산호");
     System.out.println(Hello.sum(10));
     System.out.println(Hello.sum(100));
+    System.out.println(Hello.sum(1000));
   }
 }
